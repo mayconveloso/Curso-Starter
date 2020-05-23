7706dd18bbb4eba2634fa2d127d6ddc491cd7ae1
mayconveloso <mayconveloso1@outlook.com>
2020-05-23 01:03:43 -0300

    React js
    React js

---
+++ React js/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Routes from './routes';
+import api from './services/api';
+import './styles.css';
+import Header from './components/Header';
+import Main from './pages/main';
+
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <Routes />
+    </div>
+  );
+}
+
+export default App;
